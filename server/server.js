de600369b558bc62b7b4ce2adb029c9a6579b7c3
Macintosh HD <EL-CAPITAN-2016@Mackintoshs-MacBook-Pro-4.local>
2018-03-17 19:24:20 -0400

    new features

--- server/server.js
+++ server/server.js
@@ -1,50 +1,35 @@
-
-//To get to the public it is easier to use a builtin module
-//console.log(__dirname + /../public);
-//console.log();
-
 const path = require('path');
-const publicPath = path.join(__dirname, '../public');
-const express = require('express');
-const port = process.env.PORT || 3000;
-const socket = require('socket.io');
-
 const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
 var app = express();
-//A middleware is basically a function that will the receive the Request and Response objects,
-//just like your route Handlers do.
-
-//As a third argument you have another function which you should call once your middleware code completed.
-
 var server = http.createServer(app);
-//this is how we are communicating with our server
-var io = socket(server);
-
-
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-//we registeer an event and listen to it
+io.on('connection', (socket) => {
+  console.log('New user connected');
 
-io.on('connection', (socket) =>{
-  console.log('new user connected');
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-  socket.on('createMessage', (newMessage) =>{
-    console.log('createMessage', newMessage);
-    io.emit('newMessage',{
-      from:newMessage.from,
-      text:newMessage.text,
-      createdAt: new Date().getTime()
-    });
+  socket.on('createMessage', (message, callback) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    callback('this is from the server');
   });
 
-  socket.on('disconnect', () =>{
-    console.log('user was disconnected');
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
   });
 });
 
 server.listen(port, () => {
-    console.log(`Started up at port ${port}`);
-  });
+  console.log(`Server is up on ${port}`);
+});
