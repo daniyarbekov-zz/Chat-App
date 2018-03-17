703cf2332cd02e0f1b5e8a9462692c6125c818b1
Macintosh HD <EL-CAPITAN-2016@Mackintoshs-MacBook-Pro-4.local>
2018-03-17 18:03:03 -0400

    emmit message on create Message

--- server/server.js
+++ server/server.js
@@ -7,15 +7,44 @@ const path = require('path');
 const publicPath = path.join(__dirname, '../public');
 const express = require('express');
 const port = process.env.PORT || 3000;
+const socket = require('socket.io');
+
+const http = require('http');
 
 var app = express();
 //A middleware is basically a function that will the receive the Request and Response objects,
 //just like your route Handlers do.
 
 //As a third argument you have another function which you should call once your middleware code completed.
+
+var server = http.createServer(app);
+//this is how we are communicating with our server
+var io = socket(server);
+
+
+
 app.use(express.static(publicPath));
 
+//we registeer an event and listen to it
 
-app.listen(port, () => {
+io.on('connection', (socket) =>{
+  console.log('new user connected');
+
+
+  socket.on('createMessage', (newMessage) =>{
+    console.log('createMessage', newMessage);
+    io.emit('newMessage',{
+      from:newMessage.from,
+      text:newMessage.text,
+      createdAt: new Date().getTime()
+    });
+  });
+
+  socket.on('disconnect', () =>{
+    console.log('user was disconnected');
+  });
+});
+
+server.listen(port, () => {
     console.log(`Started up at port ${port}`);
   });
